8bbbec2e872afc66f8bf9fb0cf285ea599412351
iabdrashitov <ildar.abdrashitov@introhive.com>
2021-08-24 10:03:17 -0300

    minor refactor

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from src import logging
 
 if __name__ == "__main__":
+    logging.info('Hello World')
     df = pd.DataFrame()
     df['test'] = [1, 2, 3]
     df.to_csv('test.csv')
